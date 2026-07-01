0100fd98d03975afb2d8a3aeff073b430ea0c312
Shardul kc <kcey2814@gmail.com>
2026-07-01 10:53:57 +0530

    implement product managing service using Arraylist collection

---
+++ src/com/inventory/service/ProductManager.java
@@ -0,0 +1,34 @@
+package com.inventory.service;
+
+import com.inventory.model.Product;
+import java.util.ArrayList;
+
+public class ProductManager {
+    private final ArrayList<Product> stockList;
+
+    public ProductManager() {
+        this.stockList = new ArrayList<>();
+    }
+    public ProductManager(ArrayList<Product> stockList) {
+        this.stockList = stockList;
+    }
+    public void addProduct(Product product) {
+        stockList.add(product);
+        System.out.println("✅ Success: " + product.getName() + " added to stock.");
+    }
+    public void viewInventory() {
+        if (stockList.isEmpty()) {
+            System.out.println("⚠️ Notice: The inventory is currently empty.");
+            return;
+        }
+
+        System.out.println("\nCURRENT INVENTORY");
+        System.out.printf("%-10s %-25s %-10s%n", "ID", "PRODUCT NAME", "QUANTITY");
+        System.out.println("---------------------------------------------------");
+
+        for (Product p : stockList) {
+            System.out.printf("%-10d %-25s %-10d%n", p.getId(), p.getName(), p.getQuantity());
+        }
+        System.out.println(" \n");
+    }
+}
