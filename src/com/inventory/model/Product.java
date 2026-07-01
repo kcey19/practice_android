e1224c6f41833b73dc8f7ec2ae5a2a358e5d451d
Shardul kc <kcey2814@gmail.com>
2026-07-01 10:12:29 +0530

    create encapsulated product model with attributes getters and setters

---
+++ src/com/inventory/model/Product.java
@@ -0,0 +1,28 @@
+package com.inventory.model;
+
+public class Product{
+    private final int id;
+    private final String name;
+    private int quantity;
+
+    // constructors
+    public Product(int id,String name,int quantity){
+        this.id = id;
+        this.name=name;
+        this.quantity=quantity;
+    }
+
+    // getters and setters method
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+}
